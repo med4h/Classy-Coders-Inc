fa6b0b834788dc9e9a4f823910d1c367ee627e02
med4h <adrian.meda6@gmail.com>
2024-10-29 16:07:38 -0700

    added Employee and Manager

--- classes/Employee.js
+++ classes/Employee.js
@@ -1,10 +1,35 @@
 class Employee {
-
-
-
-    
+    static #allEmployees = [];
+    #salary
+    #isHired
+    constructor(name, position, salary, isHired = true){
+    this.name = name;
+    this.position = position;
+    this.#salary = salary;
+    this.#isHired = isHired;
+}
+getSalary(){
+    return this.#salary;
+}
+getSalaryAmount(amount){
+   this.#salary + amount;
+}
+getStatus(){
+    return this.#isHired;
+}
+setStatus(command){
+if(command !== 'hire')
+    return false
+}
+static getEmployees() {
+    return Employee.#allEmployees;
 }
 
+static getTotalPayroll() {
+    return Employee.#allEmployees.reduce((total, employee) => total + employee.getSalary(), 0);
+
+}
+}
 module.exports = {
     Employee,
 }
--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,8 +1,19 @@
-class SalesPerson {
+const { Employee } = require("./Employee");
 
 
-
-    
+class SalesPerson extends Employee {
+    #totalSales
+constructor(name, position, salary, isHired, clients = [], totalSales = 0){
+    super(name, position, salary, isHired);
+    this.clients = clients;
+    this.#totalSales = totalSales;
+}
+getSalesNumbers(){
+    return this.#totalSales
+}
+makeSale(amount){
+    this.#totalSales + amount
+}
 }
 
 module.exports = {
